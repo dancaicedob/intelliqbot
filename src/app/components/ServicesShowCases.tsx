79ce77ce56b470ea8de84bf18abe9736808cf8c1
Daniel Caicedo <79131464+danielcaicedo95@users.noreply.github.com>
2025-05-02 19:36:49 -0500

    nuevas graficas

--- src/app/components/ServicesShowCases.tsx
+++ src/app/components/ServicesShowCases.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import { BoxIcon, Settings2Icon, SignalIcon, BotIcon } from 'lucide-react';
 import CartoonChatbotScene from '@/app/components/home/iconos/CartoonBot';
 import InventoryDashboard from './home/iconos/InventoryDashboardIcon';
 import BusinessFlow from './home/iconos/BusinessFlow';
