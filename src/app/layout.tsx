263e32719dca9379cc3f70f7b18bdca352fed53a
Daniel Caicedo <79131464+danielcaicedo95@users.noreply.github.com>
2026-03-31 18:57:17 -0500

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,37 +47,10 @@ export default async function RootLayout({
       <head>
         {/* Preconnect to critical resources - only if used */}
         {scripts.gtm_id && <link rel="preconnect" href="https://www.googletagmanager.com" />}
-        
-        {/* GTM Script en head */}
-        {scripts.gtm_id && (
-          <Script
-            id="gtm-head"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${scripts.gtm_id}');`
-            }}
-          />
-        )}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* GTM NoScript - DEBE ser lo primero en body */}
-        {scripts.gtm_id && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${scripts.gtm_id}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-        )}
-        
         {children}
         
         {scripts.gtm_id && <GoogleTagManager gtmId={scripts.gtm_id} />}
